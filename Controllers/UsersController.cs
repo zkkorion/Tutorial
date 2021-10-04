f8cbb1372d7e0cae03e0fb3a63562b25701446bf
Zolotarev Konstantin <KZolotarev@htsts.ru>
2021-10-04 13:13:58 +0500

    Добавьте файлы проекта.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Tutorial.Models;
+
+namespace Tutorial.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+        UsersContext db;
+        public UsersController(UsersContext context)
+        {
+            db = context;
+            if (!db.Users.Any())
+            {
+                db.Users.Add(new User { Name = "Tom", Age = 26 });
+                db.Users.Add(new User { Name = "Alice", Age = 31 });
+                db.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> Get()
+        {
+            return await db.Users.ToListAsync();
+        }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> Get(int id)
+        {
+            User user = await db.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if (user == null)
+                return NotFound();
+            return new ObjectResult(user);
+        }
+
+        // POST api/users
+        [HttpPost]
+        public async Task<ActionResult<User>> Post(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        // PUT api/users/
+        [HttpPut]
+        public async Task<ActionResult<User>> Put(User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            if (!db.Users.Any(x => x.Id == user.Id))
+            {
+                return NotFound();
+            }
+
+            db.Update(user);
+            await db.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        // DELETE api/users/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> Delete(int id)
+        {
+            User user = db.Users.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            db.Users.Remove(user);
+            await db.SaveChangesAsync();
+            return Ok(user);
+        }
+    }
+}
---
+++ Models/UsersContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tutorial.Models
+{
+	public class UsersContext : DbContext
+	{
+        public DbSet<User> Users { get; set; }
+        public UsersContext(DbContextOptions<UsersContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
